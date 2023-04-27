27710c2361983a0311a3307ea2b1a224938cd4d5
kocaserife <105380391+kocaserife@users.noreply.github.com>
2023-04-27 22:10:50 +0300

    Add files via upload

---
+++ MaasHesaplayici/src/fghjk.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+class Function {
+    String CheckNums(int num1, int num2) {
+        if(num1==num2)
+            return "-1";
+        if(num1<num2)
+            return "true";
+        return "false";
+    }
+
+
+    px
+}
+
+class Main {
+    public static void main (String[] args) {
+
+        // keep this function call here
+        Scanner  s = new Scanner(System.in);
+        Function c = new Function();
+        System.out.print(c.CheckNums(s.nextLine()));
+
+    }
+}
+
+
+
+
+}
